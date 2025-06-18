975dcd7a63b300f00e7144c3221ea38d31b8d184
afzal007ahmed <afzalahmeddc@gmail.com>
2025-06-18 21:54:17 +0530

    changes

--- src/pages/SingleWeatherPage/components/Map.tsx
+++ src/pages/SingleWeatherPage/components/Map.tsx
@@ -1,7 +1,10 @@
+import type { RootState } from "@/store/RootReducer"
 import type { LatLngExpression } from "leaflet"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import { useSelector } from "react-redux"
 
 const Map = ({ center } : { center : LatLngExpression }) => {
+  const forecast = useSelector(( state : RootState  ) => state.reducerForecast ) ;
   return (
     <div className="h-[200px]">
         <MapContainer center={ center } zoom={13} scrollWheelZoom ={ false } dragging={false} className="w-full h-full rounded-lg" >
@@ -10,7 +13,7 @@ const Map = ({ center } : { center : LatLngExpression }) => {
            />
            <Marker position={center}>
             <Popup>
-                { document.title } 
+                { forecast.data?.city.name } 
             </Popup>
            </Marker>
         </MapContainer>
